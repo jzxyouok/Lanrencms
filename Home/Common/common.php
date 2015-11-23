533d7270656f51b3524c730bf34a163044c0aa7a
bruce <test11798@gmail.cm>
2015-11-23 11:14:00 +0800

    mac

--- Home/Common/common.php
+++ Home/Common/common.php
@@ -153,6 +153,118 @@ function httpPost($url,$params)
 
 
 
+/**
+ *  中文截取2，单字节截取模式
+ *
+ * @access    public
+ * @param     string  $str  需要截取的字符串
+ * @param     int  $slen  截取的长度
+ * @param     int  $startdd  开始标记处
+ * @return    string
+ */
+
+    function cn_substr($str, $slen, $startdd=0,$soft_lang='utf-8')
+    {
+        if($soft_lang=='utf-8')
+        {
+            return cn_substr_utf8($str, $slen, $startdd);
+        }
+        $restr = '';
+        $c = '';
+        $str_len = strlen($str);
+        if($str_len < $startdd+1)
+        {
+            return '';
+        }
+        if($str_len < $startdd + $slen || $slen==0)
+        {
+            $slen = $str_len - $startdd;
+        }
+        $enddd = $startdd + $slen - 1;
+        for($i=0;$i<$str_len;$i++)
+        {
+            if($startdd==0)
+            {
+                $restr .= $c;
+            }
+            else if($i > $startdd)
+            {
+                $restr .= $c;
+            }
+
+            if(ord($str[$i])>0x80)
+            {
+                if($str_len>$i+1)
+                {
+                    $c = $str[$i].$str[$i+1];
+                }
+                $i++;
+            }
+            else
+            {
+                $c = $str[$i];
+            }
+
+            if($i >= $enddd)
+            {
+                if(strlen($restr)+strlen($c)>$slen)
+                {
+                    break;
+                }
+                else
+                {
+                    $restr .= $c;
+                    break;
+                }
+            }
+        }
+        return $restr;
+    }
+
+/**
+ *  utf-8中文截取，单字节截取模式
+ *
+ * @access    public
+ * @param     string  $str  需要截取的字符串
+ * @param     int  $slen  截取的长度
+ * @param     int  $startdd  开始标记处
+ * @return    string
+ */
+    function cn_substr_utf8($str, $length, $start=0)
+    {
+        if(strlen($str) < $start+1)
+        {
+            return '';
+        }
+        preg_match_all("/./su", $str, $ar);
+        $str = '';
+        $tstr = '';
+
+        //为了兼容mysql4.1以下版本,与数据库varchar一致,这里使用按字节截取
+        for($i=0; isset($ar[0][$i]); $i++)
+        {
+            if(strlen($tstr) < $start)
+            {
+                $tstr .= $ar[0][$i];
+            }
+            else
+            {
+                if(strlen($str) < $length + strlen($ar[0][$i]) )
+                {
+                    $str .= $ar[0][$i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return $str;
+    }
+
+
+
+
 
 //递归 获取分类
 function getJG($id=0,$reid=0,$tt="",$select=""){
--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -96,6 +96,94 @@ class IndexAction extends Action {
 	}
 
 
+public function search(){
+
+				if(!empty($_GET['keyword'])){
+					 $map['title'] = array('like',"%".$this->_param('keyword')."%"); 
+	 				$this->assign ( 'keyword',  $this->_param('keyword') );
+				}else{
+					$this->error('关键字不能为空！');					
+				}
+
+			    $map1['arcrank'] = array('eq',0);
+			    $map2 = array_merge($map1, $map);
+
+			    //读取数据库模块列表生成菜单项
+				$model = M ("Archives" );
+
+				//排序字段 默认为主键名
+				if (!empty ( $_REQUEST ['_order'] )) {
+					$order = $_REQUEST ['_order'];
+				} else {
+					$order = ! empty ( $sortBy ) ? $sortBy : $model->getPk ();
+				}
+				//排序方式默认按照倒序排列
+				//接受 sost参数 0 表示倒序 非0都 表示正序
+				if (isset ( $_REQUEST ['_sort'] )) {
+		//			$sort = $_REQUEST ['_sort'] ? 'asc' : 'desc';
+					$sort = $_REQUEST ['_sort'] == 'asc' ? 'asc' : 'desc'; //zhanghuihua@msn.com
+				} else {
+					$sort = $asc ? 'asc' : 'desc';
+				}
+				//取得满足条件的记录数
+				$_REQUEST ['listRows'] = 20;
+				$count = $model->where ( $map2 )->count ();
+				if ($count > 0) {
+					import ( "@.ORG.Util.Page" );
+					//创建分页对象
+					if (! empty ( $_REQUEST ['listRows'] )) {
+						$listRows = $_REQUEST ['listRows'];
+					} else {
+						$listRows = '';
+					}
+					$p = new Page ( $count, $listRows );
+					//分页查询数据
+					$voList = $model->where($map2)->order( "`" . $order . "` " . $sort)->limit($p->firstRow . ',' . $p->listRows)->select ( );
+					//echo $model->getlastsql();
+					//分页跳转的时候保证查询条件
+					foreach ( $map as $key => $val ) {
+						if (! is_array ( $val )) {
+							$p->parameter .= "$key=" . urlencode ( $val ) . "&";
+						}
+					}
+					//分页显示
+					$page = $p->show ();
+					//列表排序显示
+					$sortImg = $sort; //排序图标
+					$sortAlt = $sort == 'desc' ? '升序排列' : '倒序排列'; //排序提示
+					$sort = $sort == 'desc' ? 1 : 0; //排序方式
+
+					//模板赋值显示
+					$this->assign ( 'list', $voList );
+					$this->assign ( 'sort', $sort );
+					$this->assign ( 'order', $order );
+					$this->assign ( 'sortImg', $sortImg );
+					$this->assign ( 'sortType', $sortAlt );
+					$this->assign ( "page", $page );
+				}
+
+
+				//zhanghuihua@msn.com
+				$this->assign ( 't',  $this->_param('t') );
+				$this->assign ( 'totalCount', $count );
+				$this->assign ( 'numPerPage', $p->listRows );
+				$this->assign ( 'currentPage', !empty($_REQUEST[C('VAR_PAGE')])?$_REQUEST[C('VAR_PAGE')]:1);
+
+
+				$template = '';
+				if(!empty($_GET['typeid'])){
+					 $type = M("Arctype")->where("id=".$_GET['typeid'])->find();
+ 					 $this->assign ( 'type', $type );
+ 					 $template = str_replace(".html","",$type['templist']);
+
+				}
+				$this->display ($template);				
+
+	}
+
+
+
+
     public function article(){
 		$model = D ( "Archives" );
         $id = $_REQUEST ["id"];
