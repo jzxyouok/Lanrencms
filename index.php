09235630ef5e795b3bba4c1ca70aa8852753ee89
bruce <test11798@gmail.cm>
2015-11-24 16:34:09 +0800

    55555

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ define('APP_DEBUG', true); //调试模式开关
 define('APP_PATH', './Home/'); //定义应用目录
  
 
-// 加载框架入口文件 wang-mac branch
+// 加载框架入口文件 wang-mac branch  111
 require_once ("./ThinkPHP/ThinkPHP.php");
 
 
