9e0c634e953e862c4557fa2c63e6ca44012a0586
bruce994 <26wz.wang@gmail.com>
2015-11-28 14:43:45 +0800

    centos

--- Admin/Conf/config.php
+++ Admin/Conf/config.php
@@ -10,7 +10,7 @@ $config = array (
    * 2:REWRITE模式(PATHINFO模式基础上隐藏index.php)
    * 3:兼容模式(普通模式和PATHINFO模式, 可以支持任何的运行环境, 如果你的环境不支持PATHINFO 请设置为3)
    */
-
+'URL_MODEL' => 1,
 
   'TMPL_ACTION_ERROR' => './view/msg.html',
 
